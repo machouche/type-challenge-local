867f6f1a731977f9b2e7ee84692a5d91566f2d10
Maxime Achouche <maxime.achouche@gmail.com>
2023-01-08 18:52:20 +0100

    00015-undefined-last-of-array

--- challenges/00015-undefined-last-of-array.ts
+++ challenges/00015-undefined-last-of-array.ts
@@ -24,15 +24,16 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Last<T extends any[]> = any
+type Last<T extends any[]> = T extends [...any, infer L] ? L : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
+  Expect<Equal<Last<[3]>, 3>>,
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
-  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
-]
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>
+];
 
 /* _____________ Further Steps _____________ */
 /*
